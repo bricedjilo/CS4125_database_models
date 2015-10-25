33cb450556839b27f000a3d7a3c4a356cb63d1b4
Brice Djilo <dbrice@uno.edu>
2015-10-25 18:46:59 -0500

    Adds ..course.sql, ..prerequisite.sql, ..project.sql

---
+++ scripts/scripts_prerequisite.sql
@@ -0,0 +1,68 @@
+BEGIN
+	EXECUTE IMMEDIATE 'DROP TABLE prerequisite';
+  EXCEPTION
+	WHEN OTHERS THEN NULL;
+END;
+/
+
+CREATE TABLE prerequisite(
+	c_code				varchar(10) NOT NULL,
+	prerequisite_id		varchar(10) NOT NULL,
+	co_prerequisite_id	varchar(10) NOT NULL,
+	primary key (c_code, prerequisite_id),
+	foreign key (c_code) references course_
+		on delete cascade
+);
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id) 
+VALUES ('CSCI1581','None','CSCI1583');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id) 
+VALUES ('CSCI1583','MATH1115','CSCI1581');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id) 
+VALUES ('CSCI2120','CSCI1581','CSCI2121');
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id) 
+VALUES ('CSCI2120','CSCI1583','CSCI2121');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2121','None','CSCI2120');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2125','CSCI2120','None');
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2125','CSCI2121','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2450','CSCI1583','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2467','CSCI2120','None');
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI2467','CSCI2450','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI3301','CSCI2120','None');
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI3301','CSCI2450','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4101','CSCI2125','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4125','CSCI2125','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)   
+VALUES ('CSCI4208','CSCI2125','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4350','CSCI2125','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4401','CSCI2125','None');
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4401','CSCI2467','None');
+
+INSERT INTO prerequisite (c_code,prerequisite_id,co_prerequisite_id)  
+VALUES ('CSCI4402','CSCI4101','None');
+
