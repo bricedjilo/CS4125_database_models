411a9f4c3cfe599bf9d18ef33f784f40f6d73adc
Brice Djilo <dbrice@uno.edu>
2015-10-31 17:05:02 -0500

    Changes level to skill_level in knowledgeSkill.sql

--- scripts/scripts_knowledgeSkill.sql
+++ scripts/scripts_knowledgeSkill.sql
@@ -1,7 +1,7 @@
 BEGIN
-	EXECUTE IMMEDIATE 'DROP TABLE knowledgeSkill';
-  EXCEPTION
-	WHEN OTHERS THEN NULL;
+	EXECUTE IMMEDIATE 'DROP TABLE knowledgeSkill CASCADE CONSTRAINTS'; 		
+	EXECUTE IMMEDIATE 'DROP TABLE knowledgeSkill PURGE'; 	
+	EXCEPTION WHEN OTHERS THEN NULL;
 END;
 /
 
@@ -9,43 +9,43 @@ CREATE TABLE knowledgeSkill (
   ks_code 		varchar(10) NOT NULL,
   title 		varchar(100) NOT NULL,
   description 	varchar(300) NOT NULL,
-  level 		varchar(10) NOT NULL,
+  skill_level 		varchar(15) NOT NULL,
   PRIMARY KEY(ks_code)
 );
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('COM2001','Effective Technical Writing',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('COM2001','Effective Technical Writing',
 'Create a full range of technical documents with solid structures 
 Use templates to quickly start the writing process 
 Explore techniques for getting past writer’s block','Beginner');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('COM2002','Communicate with Diplomacy, Tact and Credibility',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('COM2002','Communicate with Diplomacy, Tact and Credibility',
 'Apply diplomacy and tact to be a credible and effective communicator 
 Manage the impact your communications have on your image 
 Define and leverage your communication style','Advanced');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('LFS1001','Introduction to Linux',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('LFS1001','Introduction to Linux',
 'Linux powers 94% of the world’s supercomputers, most of the servers powering the Internet, the majority 
 of financial trades worldwide and a billion Android devices. In short, Linux is everywhere. It appears in 
 many different architectures, from mainframes to server to desktop to mobile and on a staggeringly wide 
 variety of hardware. This free self-paced course will teach you how to develop a good working knowledge of 
 Linux using both the graphical interface and command line','Beginner');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('LFS1002','Fundamentals of Linux',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('LFS1002','Fundamentals of Linux',
 'Linux is everywhere and is becoming more and more ubiquitous every day. This live, instructor-led course 
 will help you acquire a practical understanding of how Linux works, quickly get up to speed using the 
 Linux graphical interface and leverage efficiencies by using the command line tools.','Beginner');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('LFS2001','Essentials of System Administration',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('LFS2001','Essentials of System Administration',
 'Linux is the #1 operating system for web servers, cloud computing, smart phones and consumer electronics. Due to its 
 rapid adoption, theres a shortage of Linux system administrators. According to the 2015 Linux Jobs Report, "the unstoppable 
 Linux job market shows no sign of slowing down." The Linux Foundation has created a career entry training and certification 
 path that is 100% online, highly respected and exceptionally affordable.','Beginner');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('WDFE1001','Introduction to Web Design','You will replicate a design mockup in HTML and CSS. 
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('WDFE1001','Introduction to Web Design','You will replicate a design mockup in HTML and CSS. 
 You will develop a responsive website that will display images, descriptions and links to each of the portfolio projects you will 
 complete throughout the course of the Front-End Web Developer.','Beginner');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('DBAS2002','Database Concurrency Control',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('DBAS2002','Database Concurrency Control',
 'Transaction Processing and Concurrency Control:Transaction Fundamentals (3 sessions): OLTP environments, Concurrency 
 issues, need for transactions, Necessary properties of transactions (ACID properties), Transaction states, serializability, 
 Serial schedules, Conflict serializability, View serializability, Recoverable and non-recoverable schedules, Cascading rollbacks, 
@@ -56,7 +56,7 @@ Multi-granularity locking, Deadlock prevention protocols, Wait-die and wound-wai
 Deadlock recovery, Nested transactions - Database recovery techniques:Recovery concepts, Deferred updates technique, 
 Immediate update technique, Shadow paging, ARIES recovery algorithm','Advance');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JDBC1001','JDBC Programming',
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JDBC1001','JDBC Programming',
 'The course starts with an overview of the JDBC API. Then you learn how to set up your development 
 environment with the appropriate MySQL database drivers. Next the course shows you how to submit a SQL query and process the 
 result set. Also, you learn how to perform SQL insert, updates and deletes.
@@ -65,25 +65,25 @@ procedures using various parameter types (IN, INOUT, OUT and ResultSet). Next, t
 types such as BLOBs and CLOBs. Finally, the course wraps up with a section on reading database connection information from a 
 configuration file.','Medium');
 
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('RDBMS1001','RDBMS Design','nec, diam.','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('CSS1002','CSS','non, sollicitudin','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('RUBY1001','Ruby','volutpat nunc','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('DBAS1001','Data Modeling','Sed neque.','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('DBAS1002','SQL','sodales. Mauris','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('FIN2002',' Financial Analysis','nunc ac','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('COM1001','Composition','semper, dui','Advanced');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('FIN3000','Financial Software','velit dui,','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('ACC1001','Bookkeeping','tristique neque','Advanced');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('DBAS1003','Relational Models','ac turpis','Advanced');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVS1001','Javascript','pretium aliquet,','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('DBAS3003','Relational Object Database','interdum. Curabitur','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVS1002','Javascript','ut, nulla.','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVA1001','Core Java','ut, nulla.','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVA1002','Core Java','ut, nulla.','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVA2001','Core Java','ut, nulla.','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('JAVA2002','Core Java','ut, nulla.','Advanced');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('ALGB1001','Algebra I','ut, nulla.','Beginner');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('ALGB1002','Algebra II','ut, nulla.','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('ALGB1003','Algebra III','ut, nulla.','Advanced');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('COMP2001','Computer Hardware','ut, nulla.','Medium');
-INSERT INTO knowledgeSkill (ks_code,title,description,level) VALUES ('COMP2002','Computer Software - Algorithms','ut, nulla.','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('RDBMS1001','RDBMS Design','nec, diam.','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('CSS1002','CSS','non, sollicitudin','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('RUBY1001','Ruby','volutpat nunc','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('DBAS1001','Data Modeling','Sed neque.','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('DBAS1002','SQL','sodales. Mauris','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('FIN2002',' Financial Analysis','nunc ac','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('COM1001','Composition','semper, dui','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('FIN3000','Financial Software','velit dui,','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('ACC1001','Bookkeeping','tristique neque','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('DBAS1003','Relational Models','ac turpis','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVS1001','Javascript','pretium aliquet,','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('DBAS3003','Relational Object Database','interdum. Curabitur','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVS1002','Javascript','ut, nulla.','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVA1001','Core Java','ut, nulla.','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVA1002','Core Java','ut, nulla.','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVA2001','Core Java','ut, nulla.','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('JAVA2002','Core Java','ut, nulla.','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('ALGB1001','Algebra I','ut, nulla.','Beginner');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('ALGB1002','Algebra II','ut, nulla.','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('ALGB1003','Algebra III','ut, nulla.','Advanced');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('COMP2001','Computer Hardware','ut, nulla.','Medium');
+INSERT INTO knowledgeSkill (ks_code,title,description,skill_level) VALUES ('COMP2002','Computer Software - Algorithms','ut, nulla.','Advanced');
