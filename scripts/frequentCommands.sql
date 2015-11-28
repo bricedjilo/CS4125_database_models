ca2949da2a597c4ef6bf52296346dd1bb651db02
Brice Djilo <dbrice@uno.edu>
2015-11-28 13:20:53 -0600

    Adds Auto-increment to scrips_company file

--- scripts/frequentCommands.sql
+++ scripts/frequentCommands.sql
@@ -1,2 +1,8 @@
+-- Get all tables for this user
 SELECT table_name from user_tables;
 select 'drop table '|| table_name ||' cascade constraints;' from user_tables;
+
+-- Install a jar in maven
+mvn install:install-file -Dfile=ojdbc7-12.1.0.2.jar -DgroupId=com.oracle -DartifactId=ojdbc7-12.1.0.2 -Dversion=12.1.0.2 -Dpackaging=jar
+
+-- 
