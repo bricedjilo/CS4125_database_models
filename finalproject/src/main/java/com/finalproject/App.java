65af52e7dddf21e5f5dcf29cf107455d9ce855c4
Brice Djilo <dbrice@uno.edu>
2015-12-01 11:53:05 -0600

    pushing target

--- finalproject/src/main/java/com/finalproject/App.java
+++ finalproject/src/main/java/com/finalproject/App.java
@@ -1,6 +1,5 @@
 package com.finalproject;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +10,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.finalproject")
 public class App {
 	public static void main(String[] args) {
-		Flyway flyway = new Flyway();
-		flyway.setDataSource("jdbc:oracle:thin:@dbsvcs.cs.uno.edu:1521:orcl", "dbrice", "tWK9XNrn");
+	//	Flyway flyway = new Flyway();
+	//	flyway.setDataSource("jdbc:oracle:thin:@dbsvcs.cs.uno.edu:1521:orcl", "dbrice", "tWK9XNrn");
 	//	flyway.setBaselineOnMigrate(true);
 	//	flyway.setBaselineDescription("Baseline Version");
 	//	flyway.setSchemas("dbrice");
 		
-		flyway.migrate();
+	//	flyway.migrate();
 		SpringApplication.run(App.class, args);
 		
 	}
